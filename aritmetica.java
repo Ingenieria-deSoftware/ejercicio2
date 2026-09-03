790cbe83b9925fc4c8a344fd076e759dfbc24867
s4LeX <116523944+A-Saavedra@users.noreply.github.com>
2026-09-03 06:17:26 -0400

    Update aritmetica.java

--- aritmetica.java
+++ aritmetica.java
@@ -4,7 +4,13 @@ public class aritmetica {
     // SECCIÓN DE OPERACIONES ARITMÉTICAS
     // ==========================================
     
-    // Cada dev deve insertar su función aquí
+    /*
+     * Función: suma
+     * Autor: Carlos Mendoza
+     */
+    public static double suma(double a, double b) {
+        return a + b;
+    }
     
     public static void main(String[] args) {
         System.out.println("--- Pruebas de Operaciones ---");
