c37de0f12703cd8dd32e10c80ada27fc488804bd
A-Saavedra <alexcrisangel@gmail.com>
2026-09-03 05:52:56 -0400

    feat: archivo base aritmetica.java

---
+++ aritmetica.java
@@ -0,0 +1,12 @@
+public class aritmetica {
+
+    // ==========================================
+    // SECCIÓN DE OPERACIONES ARITMÉTICAS
+    // ==========================================
+    
+    // Cada dev deve insertar su función aquí
+    
+    public static void main(String[] args) {
+        System.out.println("--- Pruebas de Operaciones ---");
+    }
+}
