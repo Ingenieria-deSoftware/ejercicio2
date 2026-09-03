41274bfa095759dc195fc85aff679414c3fab257
A-Saavedra <alexcrisangel@gmail.com>
2026-09-03 06:14:23 -0400

    feat: implementada funcion porcentaje

--- aritmetica.java
+++ aritmetica.java
@@ -4,9 +4,15 @@ public class aritmetica {
     // SECCIÓN DE OPERACIONES ARITMÉTICAS
     // ==========================================
     
-    // Cada dev deve insertar su función aquí
-    
+    /*
+    * Función: porcentaje
+    * Autor: Alex Saavedra
+    */
+    public static double porcentaje(double total, double porcentaje) {
+        return (total * porcentaje) / 100.0;
+    }
     public static void main(String[] args) {
         System.out.println("--- Pruebas de Operaciones ---");
+        System.out.println("Porcentaje: " + porcentaje(90, 12));
     }
 }
