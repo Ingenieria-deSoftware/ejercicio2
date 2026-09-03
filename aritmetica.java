1a7924755fd115b364855ca9f552614046adbc46
diegomo669 <dieguito123muchomejor@hotmail.com>
2026-09-03 12:16:25 -0400

    feat/operacion-suma

--- aritmetica.java
+++ aritmetica.java
@@ -3,7 +3,13 @@ public class aritmetica {
     // ==========================================
     // SECCIÓN DE OPERACIONES ARITMÉTICAS
     // ==========================================
-    
+      /*
+    * Función: SUMA
+    * Autor: Diego alejandro montaño quispe
+    */
+      public static double suma(int num1,int num2 ) {
+        return (num1 + num2);
+      }
     /*
     * Función: porcentaje
     * Autor: Alex Saavedra
@@ -13,6 +19,7 @@ public class aritmetica {
     }
     public static void main(String[] args) {
         System.out.println("--- Pruebas de Operaciones ---");
+         System.out.println("Suma: " + suma(10, 10));
         System.out.println("Porcentaje: " + porcentaje(90, 12));
     }
 }
